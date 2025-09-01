98c62601c51e74f5fc1ebbaced4c24428e2a4aae
Lucieran-Raven <lucieranraven@gmail.com>
2025-09-01 21:51:19 +0800

    "Day 1: Core engine online. Tensor + autograd."

---
+++ core/tensor.py
@@ -0,0 +1,34 @@
+# core/tensor.py
+# The core of Titan-X: Autograd Engine
+
+class Tensor:
+    def __init__(self, data):
+        self.data = data
+        self.grad = 0
+        self._backward = lambda: None
+        self._prev = set()
+
+    def __add__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data + other.data)
+        
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data * other.data)
+        
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        self.grad = 1.0
+        # In real version, we'll add topological sort
+        # For now, simple case
